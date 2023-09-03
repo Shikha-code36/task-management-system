7fae67405796390438d54895b4156b19a45762ca
Shikha <shikha.py36@gmail.com>
2023-09-03 18:21:07 +0530

    path to APIs

---
+++ task_manager/tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task/new/', views.task_create, name='task_create'),
+    path('task/<int:pk>/edit/', views.task_edit, name='task_edit'),
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
+]
